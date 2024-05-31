d0fe2a1a30cb1bf10b27f0897b11a8e048cb7d9a
Faiz Ali33 <faiz0651@gmail.com>
2024-06-01 01:36:36 +0500

    add chalk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 import chalk from "chalk";
 console.log(chalk.yellow("\n<<<<< Wellcome To The FaizCode Quiz-Project >>>>>\n"));
